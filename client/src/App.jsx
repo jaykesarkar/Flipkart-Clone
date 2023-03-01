2d5977d82de594fb8a90b6cf8f33de1d1ba5295b
Jay <kesarkarjay9@gmail.com>
2023-03-01 12:50:08 +0530

    updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,22 @@
 import React from "react";
-import { Header } from "./components/header/Header";
-import Home from "./components/home/Home";
 import { Box } from "@mui/material";
 import { DataProvider } from "./context/Context";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Header } from "./components/header/Header";
+import Home from "./components/home/Home";
+import DetailView from "./components/productPage/DetailView";
 
 const App = () => {
 	return (
 		<DataProvider>
-			<Header />
-			<Box style={{ marginTop: 56 }}></Box>
-			<Home />
+			<BrowserRouter>
+				<Header />
+				<Box style={{ marginTop: 56 }}></Box>
+				<Routes>
+					<Route path="/" element={<Home />} />
+					<Route path="/product/:id" element={<DetailView />} />
+				</Routes>
+			</BrowserRouter>
 		</DataProvider>
 	);
 };

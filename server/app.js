5d71e7196771044128d0e536132c707f72450c38
Jay <kesarkarjay9@gmail.com>
2023-03-09 23:41:26 +0530

    new changes

--- server/app.js
+++ server/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import bodyParser from "body-parser";
 import cors from "cors";
+import path from "path";
 
 const app = express();
 import router from "./routes/routes.js";
@@ -10,7 +11,17 @@ app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors());
 
+// serving the frontend
+app.use(express.static(path.join(_dirname, "./client/build")));
+
+app.get("*", (req, res) => {
+	res.sendFile(path.join(_dirname, "./frontend/build/index.html")),
+		function (err) {
+			res.status(500).send(err);
+		};
+});
+
 // Routes
-app.use("/allRoutes", router);
+app.use("/", router);
 
 export default app;

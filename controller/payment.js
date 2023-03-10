a2291978bc9a114f2b62892b0fa4c5c4b0c9977c
Jay <kesarkarjay9@gmail.com>
2023-03-10 13:24:18 +0530

    commit

--- controller/payment.js
+++ controller/payment.js
@@ -61,7 +61,7 @@ export const paymentResponse = (request, response) => {
 					post_res.on("end", function () {
 						let result = JSON.parse(res);
 						// console.log(result);
-						response.redirect(`http://localhost:8000/`);
+						response.redirect(`http://localhost:8000`);
 					});
 				});
 				post_req.write(post_data);

7f2524bee5a5052fff324df8c4b92a37d5819710
Jay <kesarkarjay9@gmail.com>
2023-03-10 12:39:25 +0530

    commit

--- app.js
+++ app.js
@@ -18,7 +18,7 @@ app.use(cors());
 app.use("/", router);
 
 // Serving Frontend(client)
-app.use(express.static(path.join(__dirname, "client/build")));
+app.use(express.static(path.join(__dirname + "client/build")));
 app.get("*", (req, res) => {
 	res.sendFile(path.join(__dirname + "/client/build/index.html"));
 });

3ad2fcc56c9919987a51346da03dc3460d0c000b
Jay <kesarkarjay9@gmail.com>
2023-03-11 23:39:18 +0530

    commit

--- server.js
+++ server.js
@@ -34,7 +34,8 @@ export let paytmParams = {};
 	(paytmParams["ORDER_ID"] = uuid()),
 	(paytmParams["CUST_ID"] = process.env.PAYTM_CUST_ID),
 	(paytmParams["TXN_AMOUNT"] = "100"),
-	(paytmParams["CALLBACK_URL"] = "/callback");
+	(paytmParams["CALLBACK_URL"] =
+		"https://flipkart-clone-d124.onrender.com/callback");
 paytmParams["EMAIL"] = "johnsmith@gmail.com";
 paytmParams["MOBILE_NO"] = "1234567852";
 

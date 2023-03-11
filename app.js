d216e46224b82841e44ed0141424db36a049aefc
Jay <kesarkarjay9@gmail.com>
2023-03-11 23:18:46 +0530

    commit

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
-// import express from "express";
-// import bodyParser from "body-parser";
-// import cors from "cors";
-// import path from "path";
-// import { fileURLToPath } from "url";
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = path.dirname(__filename);
+import express from "express";
+import bodyParser from "body-parser";
+import cors from "cors";
+import path from "path";
+import { fileURLToPath } from "url";
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
-// const app = express();
-// import router from "./routes/routes.js";
+const app = express();
+import router from "./routes/routes.js";
 
-// // Middlewares
-// app.use(bodyParser.json({ extended: true }));
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(cors());
+// Middlewares
+app.use(bodyParser.json({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(cors());
 
-// // Routes
-// app.use("/api", router);
+// Routes
+app.use("/", router);
 
-// // Serving Frontend(client)
-// app.use(express.static(path.join(__dirname, "./client/build")));
-// app.get("*", (req, res) => {
-// 	res.sendFile(path.join(__dirname, "./client/build/index.html")),
-// 		function (err) {
-// 			res.status(500).send(err);
-// 			console.log(err);
-// 		};
-// });
+// Serving Frontend(client)
+app.use(express.static(path.join(__dirname, "./client/build")));
+app.get("*", (req, res) => {
+	res.sendFile(path.join(__dirname, "./client/build/index.html")),
+		function (err) {
+			res.status(500).send(err);
+			console.log(err);
+		};
+});
 
-// export default app;
+export default app;
--- client/src/redux/actions/productActions.js
+++ client/src/redux/actions/productActions.js
@@ -5,9 +5,7 @@ const url = "";
 
 export const getProducts = () => async (dispatch) => {
 	try {
-		const { data } = await axios.get(
-			`https://flipkart-clone-d124.onrender.com/products`
-		);
+		const { data } = await axios.get(`${url}/products`);
 		dispatch({ type: actionTypes.GET_PRODUCTS_SUCCESS, payload: data });
 	} catch (error) {
 		dispatch({ type: actionTypes.GET_PRODUCTS_FAIL, payload: error.response });
@@ -17,9 +15,7 @@ export const getProducts = () => async (dispatch) => {
 export const getProductDetails = (id) => async (dispatch) => {
 	try {
 		dispatch({ type: actionTypes.GET_PRODUCT_DETAILS_REQUEST });
-		const { data } = await axios.get(
-			`https://flipkart-clone-d124.onrender.com/product/${id}`
-		);
+		const { data } = await axios.get(`${url}/product/${id}`);
 
 		dispatch({ type: actionTypes.GET_PRODUCT_DETAILS_SUCCESS, payload: data });
 	} catch (error) {
